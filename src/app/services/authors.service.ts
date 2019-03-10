ea5ef8614eb979438204a6180b4f0b1bc980acd1
Yulia Svitukha <temp@MacBook-Pro-2.local>
2019-03-09 18:56:34 -0600

    Service for Authors is added

---
+++ src/app/services/authors.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+import { Author } from '../modules/author';
+import { AUTHORS } from '../modules/mock-authors';
+import { MessageService } from './message.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthorsService {
+
+  constructor(private messageService: MessageService) { }
+  getAuthors(): Observable<Author[]> {
+    //TODO:send message_after_fetching the Authors
+    this.messageService.add('AuthorService: fetched authors');
+    return of(AUTHORS);
+}
+}
