eb252b242ba13bb3e496528072b7fc9333bd28e7
Svetlana Deryuga <svetlana.deryuga@gmail.com>
2019-03-11 07:43:01 -0500

    working progress

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RecipesComponent} from 'src/app/components/recipes/recipes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'recipes', component: RecipesComponent }
+];
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [ RouterModule.forRoot(routes) ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
