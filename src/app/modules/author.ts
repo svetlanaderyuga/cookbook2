243dbca21e171489479b31366dda75db65c27d13
Svetlana Deryuga <svetlana.deryuga@gmail.com>
2019-04-14 12:54:51 -0500

    Cleaned up

--- src/app/modules/author.ts
+++ src/app/modules/author.ts
@@ -3,5 +3,4 @@ export class Author {
     firstName: string;
     lastName: string;
     image: string;
-
 }
--- src/app/modules/mock-recipes.ts
+++ src/app/modules/mock-recipes.ts
@@ -1,14 +1,12 @@
 import { Recipe } from './recipe';
 
 export const RECIPES: Recipe[] = [
-  { id: 11, name: 'Borsh' },
-  { id: 12, name: 'Vatrushka Korolevskaya' },
-  { id: 13, name: 'Pelmeny' },
-  { id: 14, name: 'Kotlety' },
-  { id: 15, name: 'Salat Olivie' },
-  { id: 16, name: 'Mac and Cheese' },
-  { id: 17, name: 'Bliny' },
-  { id: 18, name: 'Grenki' },
-  { id: 19, name: 'Pirozhki' },
-  { id: 20, name: 'Salat Grecheskij' }
+  { id: 11, name: 'Borsh', image: 'assets/images/recipes/borsh.jpeg' },
+  { id: 12, name: 'Vatrushka Korolevskaya', image: 'assets/images/recipes/vatrushka.jpeg' },
+  { id: 13, name: 'Pelmeny', image: 'assets/images/recipes/pelmeny.jpeg' },
+  { id: 14, name: 'Kotlety', image: 'assets/images/recipes/kotlety.jpeg' },
+  { id: 15, name: 'Salat Olivie', image: 'assets/images/recipes/olivye.jpeg' },
+  { id: 16, name: 'Mac and Cheese', image: 'assets/images/recipes/macandcheese.jpeg' },
+  { id: 17, name: 'Bliny', image: 'assets/images/recipes/bliny.jpeg' },
 ];
+
