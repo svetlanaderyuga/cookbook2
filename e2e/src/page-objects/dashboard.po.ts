a892eaf035f2dad681e34559b7e9f04dddb3e14c
Svetlana Deryuga <svetlana.deryuga@gmail.com>
2019-04-21 11:39:10 -0500

    Added test and page object classes for the Dashboard and Recipe Details

---
+++ e2e/src/page-objects/dashboard.po.ts
@@ -0,0 +1,17 @@
+import {$, $$, browser, by, element} from 'protractor';
+
+export class Dashboard {
+
+  topRecipesTitle = $('#topRecipeTitle');
+  firstRecipeDiv = element.all(by.css('div[id*=moduleRecipe]')).get(0);
+
+  async navigateToDashboard(): Promise<void> {
+    await browser.get('/dashboard');
+  }
+  async getTopRecipeTitle(): Promise<string> {
+    return await this.topRecipesTitle.getText();
+  }
+  async clickFirstRecipe(): Promise<void> {
+    return await this.firstRecipeDiv.click();
+  }
+}
