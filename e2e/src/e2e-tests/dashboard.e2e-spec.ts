e88a46dec24bc0878093054bd58766fc86efa8ad
Svetlana Deryuga <svetlana.deryuga@gmail.com>
2019-05-05 16:25:04 -0500

    Added the locators and methods to the recipeDetails po and steps to the recipe details spec

--- e2e/src/e2e-tests/dashboard.e2e-spec.ts
+++ e2e/src/e2e-tests/dashboard.e2e-spec.ts
@@ -11,10 +11,17 @@ describe('Dashboard', () => {
       'Was not taken to Dashboard page.'
     );
   });
-  it('should display the elements of the Top Recipes block', async () => {
+  it('should display the Top Recipes title', async () => {
     expect(await dashboard.getTopRecipeTitle()) .toBe(
       'Top Recipes',
       'Invalid title is displayed for Top Recipes.'
+    );
+  });
+  it('should be redirected to Recipe Details page when clicked on the first recipe', async () => {
+    await dashboard.clickFirstRecipe();
+    expect(await browser.getCurrentUrl()).toContain(
+      'recipedetails',
+      'Was not taken to Recipe Details page.'
     );
   });
 });
--- e2e/src/e2e-tests/recipes-details.e2e-spec.ts
+++ e2e/src/e2e-tests/recipes-details.e2e-spec.ts
@@ -1,7 +1,9 @@
 import { browser } from 'protractor';
 import { Dashboard } from '../page-objects/dashboard.po';
+import { RecipeDetails } from '../page-objects/recipe-details.po';
 
 const dashboard: Dashboard = new Dashboard();
+const recipeDetails: RecipeDetails = new RecipeDetails();
 
 describe('Recipe Details', () => {
   beforeAll(async () => {
@@ -11,12 +13,22 @@ describe('Recipe Details', () => {
       'Was not taken to Dashboard page.'
     );
     await dashboard.clickFirstRecipe();
-  });
-
-  it('should be redirected to Recipe Details page', async () => {
     expect(await browser.getCurrentUrl()).toContain(
       'recipedetails',
       'Was not taken to Recipe Details page.'
     );
   });
+  it('should display the right title on the recipe details page', async () => {
+    expect(await recipeDetails.getRecipeTitle()).toContain(
+      'VATRUSHKA KOROLEVSKAYA',
+      'Wrong recipe title is displayed'
+    );
+  });
+  it('should display the right id on the recipe details page', async () => {
+    expect(await recipeDetails.getRecipeId()).toContain(
+      '12',
+      'Wrong recipe id is displayed'
+    );
+  });
+
 });
